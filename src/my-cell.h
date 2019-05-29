d2dd950c508a74738ca0c7c715a6a43cdf86ed6c
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2019-05-29 20:25:21 +0200

    Refactor simulation to dynamic biodynamo version

--- src/my-cell.h
+++ src/my-cell.h
@@ -24,58 +24,48 @@ namespace bdm {
 // for EventHandler
 using NewNeuriteExtensionEvent = bdm::experimental::neuroscience::NewNeuriteExtensionEvent;
 
-
-BDM_SIM_OBJECT(MyCell, experimental::neuroscience::NeuronSoma) {
+class MyCell : public experimental::neuroscience::NeuronSoma {
   BDM_SIM_OBJECT_HEADER(MyCell, experimental::neuroscience::NeuronSoma, 1, cell_type_, can_divide_, cell_color_);
 
  public:
-  MyCellExt() {}
-  explicit MyCellExt(const std::array<double, 3>& position) : Base(position) {}
+  MyCell() : Base() {}
+
+  virtual ~MyCell() {}
+
+  explicit MyCell(const std::array<double, 3>& position) : Base(position) {}
 
   /// If MyCell divides, daughter 2 copies the data members from the mother
-  template <typename TMother>
-  MyCellExt(const CellDivisionEvent& event, TMother* mother)
-      : Base(event, mother) {
-    cell_type_[kIdx] = mother->cell_type_[mother->kIdx];
-    can_divide_[kIdx] = mother->can_divide_[mother->kIdx];
-    cell_color_[kIdx] = mother->cell_color_[mother->kIdx];
+  MyCell(const Event& event, SimObject* other, uint64_t new_oid = 0)
+      : Base(event, other, new_oid) {
+    if (event.GetId() == CellDivisionEvent::kEventId) {
+      auto* mother = static_cast<MyCell*>(other);
+      cell_type_ = mother->cell_type_;
+      can_divide_ = mother->can_divide_;
+      cell_color_ = mother->cell_color_;
+    }
   }
 
-  /// If a cell divides, daughter keeps the same state from its mother.
-  template <typename TDaughter>
-  void EventHandler(const CellDivisionEvent& event, TDaughter* daughter) {
-    Base::EventHandler(event, daughter);
-  }
-
-  /// \brief EventHandler to modify the data members of this soma
-  /// after a new neurite extension event.
-  ///
-  /// Performs the transition mother to daughter 1
-  /// \param event contains parameters for new neurite extension
-  /// \param neurite pointer to new neurite
-  /// \see NewNeuriteExtensionEvent
-  template <typename TNeurite>
-  void EventHandler(const NewNeuriteExtensionEvent& event, TNeurite* neurite) {
-    // forward to experimental::neuroscience::NeuronSoma::EventHandler
-    Base::EventHandler(event, neurite);
+  void EventHandler(const Event& event, SimObject* other1,
+                    SimObject* other2 = nullptr) {
+    Base::EventHandler(event, other1, other2);
   }
 
   // getter and setter for our new data member
-  void SetCellType(int t) { cell_type_[kIdx] = t; }
-  int GetCellType() const { return cell_type_[kIdx]; }
+  void SetCellType(int t) { cell_type_ = t; }
+  int GetCellType() const { return cell_type_; }
 
-  void SetCanDivide(int d) { can_divide_[kIdx] = d; }
-  bool GetCanDivide() const { return can_divide_[kIdx]; }
+  void SetCanDivide(int d) { can_divide_ = d; }
+  bool GetCanDivide() const { return can_divide_; }
 
-  void SetCellColor(int cell_color) { cell_color_[kIdx] = cell_color; }
-  int GetCellColor() const { return cell_color_[kIdx]; }
+  void SetCellColor(int cell_color) { cell_color_ = cell_color; }
+  int GetCellColor() const { return cell_color_; }
 
  private:
   // declare new data member and define their type
   // private data can only be accessed by public function and not directly
-  vec<int> cell_type_;
-  vec<bool> can_divide_;
-  vec<int> cell_color_;
+  int cell_type_;
+  bool can_divide_;
+  int cell_color_;
 };
 
 }  // namespace bdm
